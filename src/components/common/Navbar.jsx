a60b2c49fb7cd859d148057508ea30aa94e84db0
GANIDU2413 <ganidusahan@gmail.com>
2024-08-02 22:46:06 +0530

    bootstrap added with navbar

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,29 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import UserService from '../service/UserService';
 
 function Navbar() {
-    const isAuthenticated = UserService.isAuthenticated();
-    const isAdmin = UserService.isAdmin();
-
-
-
-    const handleLogout = () => {
-        const confirmDelete = window.confirm('Are you sure you want to logout this user?');
-        if (confirmDelete) {
-            UserService.logout();
-        }
-    };
-
 
     return (
-        <nav>
-            <ul>
-                {!isAuthenticated && <li><Link to="/">Phegon Dev</Link></li>}
-                {isAuthenticated && <li><Link to="/profile">Profile</Link></li>}
-                {isAdmin && <li><Link to="/admin/user-management">User Management</Link></li>}
-                {isAuthenticated && <li><Link to="/" onClick={handleLogout}>Logout</Link></li>}
-            </ul>
+        <nav class="navbar bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"/>
+                    Bootstrap
+                </a>
+            </div>
         </nav>
     );
 }
