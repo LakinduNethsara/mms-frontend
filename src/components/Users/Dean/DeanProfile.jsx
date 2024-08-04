ca93e0ba22dccc18d0af75e0b820c72474754dc1
Rashini Nuwangi <119997820+RashiniNuwangi@users.noreply.github.com>
2024-08-04 14:34:03 +0530

    changes

--- src/components/Users/Dean/DeanProfile.jsx
+++ src/components/Users/Dean/DeanProfile.jsx
@@ -14,13 +14,13 @@ return(
             </div>
           </div>
 
-          <div className="col mb-4"> 
+          {/* <div className="col mb-4"> 
             <div className="card text-center functionCard">
               <div className="card-body">
                 <a href="" className="btn btn-primary home-page-class-button">Results Board</a>
               </div>
             </div>
-          </div>
+          </div> */}
   </>
 )
 
