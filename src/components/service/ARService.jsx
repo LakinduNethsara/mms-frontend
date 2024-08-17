8f8cc053a3f1da55f20a402c17dab275e64f0ab3
Lakindu Nethsara <lakindunethsara378@gmail.com>
2024-08-17 12:21:11 +0530

    Create ARService.jsx

---
+++ src/components/service/ARService.jsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+class ARService {
+    static BASE_URL = "http://localhost:9090";
+
+    static async getViewMarksCourseList(level,semester,department_id) {     // API call to fetch course data
+        try {
+            const response = await axios.get(`${ARService.BASE_URL}/api/AssistantRegistrar/getViewMarksCourseList/${level}/${semester}/${department_id}`);
+            return response.data;
+        } catch (err) {
+            console.error('Error fetching data:', err);
+        }
+    }
+
+    static async getCourseListRemainingToAddToResultBoard(level,semester,department_id,result_board_id){
+        try{
+            const courseList = await axios.get(`${ARService.BASE_URL}/api/AssistantRegistrar/getCourseListRemainingToAddToResultBoard/${level}/${semester}/${department_id}/${result_board_id}`); //Get the course list that can be added to the result board from the database
+        }
+        catch(err){
+            toast.error(err.response.data.errorMessage,{autoClose:3000}); //Display the error message if an error occurs
+
+        }
+    }
+
+
+}
+
+export default ARService;
+
