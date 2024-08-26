424cd585551c09658ef9504873115b828d516f02
Lakindu Nethsara <lakindunethsara378@gmail.com>
2024-08-26 22:03:38 +0530

    Update Navbar.jsx

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -21,7 +21,7 @@ function Navbar() {
     }
 
     return (
-        <nav className="navbar bg-body-tertiary">
+        <nav className="navbar bg-body-tertiary fixed-top">
             <div className="container-fluid">
                 {
                     user != null ? 
